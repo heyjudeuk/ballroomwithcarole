c6b2869bc38cf7e543b7b229d92e724dde858f28
heyjudeuk <86838750+heyjudeuk@users.noreply.github.com>
2026-04-27 12:32:11 +0100

    Update practice page to handle month array

--- practice-evenings-data.js
+++ practice-evenings-data.js
@@ -1,12 +1,16 @@
-window.practiceEveningsData = {
-  monthLabel: "May 2026",
-  dates: [
-    "6th May",
-    "20th May"
-  ]
-  monthLabel: "June 2026",
-  dates: [
-    "10th June",
-    "24th June"
-  ]
-};
+window.practiceEveningsData = [
+  {
+    monthLabel: "May 2026",
+    dates: [
+      "6th May",
+      "20th May"
+    ]
+  },
+  {
+    monthLabel: "June 2026",
+    dates: [
+      "10th June",
+      "24th June"
+    ]
+  }
+];
